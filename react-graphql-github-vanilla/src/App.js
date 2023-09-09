a4b52ddf461e633139af832cc8ae436806f05dec
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-09-09 12:54:06 -0300

    Add axios package

--- react-graphql-github-vanilla/src/App.js
+++ react-graphql-github-vanilla/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 const TITLE = 'React GraphQL GitHub Client';
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+});
 
 class App extends Component {
   render() {
