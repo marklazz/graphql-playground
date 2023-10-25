5083f50de708bdf331a50efc6bb9943f29d89cd4
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-10-25 16:50:14 -0300

    Implement mutation with React apollo

--- react-graphql-github-apollo-starter-kit/src/Repository/RepositoryItem/index.js
+++ react-graphql-github-apollo-starter-kit/src/Repository/RepositoryItem/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Link from '../../Link';
+import gql from 'graphql-tag';
 import '../style.css';
+import { Mutation } from 'react-apollo';
+import Button from '../../Button';
+
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+    starrable {
+    id
+    viewerHasStarred
+    }
+  }
+}
+`;
+
 const RepositoryItem = ({
+  id,
   name,
   url,
   descriptionHTML,
@@ -14,35 +30,46 @@ const RepositoryItem = ({
   }) => (
   <div>
     <div className="RepositoryItem-title">
-    <h2>
-      <Link href={url}>
-        {name}
-      </Link>
-    </h2>
-    <div className="RepositoryItem-title-action">
-      {stargazers.totalCount} Stars
-    </div>
-    </div>
-      <div className="RepositoryItem-description">
-        <div
-          className="RepositoryItem-description-info"
-          dangerouslySetInnerHTML={{ __html: descriptionHTML }}
-        />
-        <div className="RepositoryItem-description-details">
-        <div>
-          {primaryLanguage && (
-            <span>Language: {primaryLanguage.name}</span>
-          )}
-        </div>
-        <div>
-          {owner && (
-            <span>
-              Owner: <a href={owner.url}>{owner.login}</a>
-            </span>
-          )}
-        </div>
+      <h2>
+        <Link href={url}>
+          {name}
+        </Link>
+      </h2>
+      <div className="RepositoryItem-title-action">
+    {!viewerHasStarred ? (
+      <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+      {(addStar, { data, loading, error }) => (
+        <Button className={'RepositoryItem-title-action'} onClick={addStar} >
+        {stargazers.totalCount} Star
+        </Button>
+      )}
+      </Mutation>
+    ) : (
+      <span>TODO: Implement Remove Star</span>
+    )}
+{/* Here comes your updateSubscription mutation */}
       </div>
     </div>
-  </div>
+    <div className="RepositoryItem-description">
+    <div
+    className="RepositoryItem-description-info"
+    dangerouslySetInnerHTML={{ __html: descriptionHTML }}
+    />
+    <div className="RepositoryItem-description-details">
+    <div>
+    {primaryLanguage && (
+      <span>Language: {primaryLanguage.name}</span>
+    )}
+    </div>
+    <div>
+    {owner && (
+      <span>
+      Owner: <a href={owner.url}>{owner.login}</a>
+      </span>
+    )}
+    </div>
+    </div>
+    </div>
+    </div>
 );
 export default RepositoryItem;
