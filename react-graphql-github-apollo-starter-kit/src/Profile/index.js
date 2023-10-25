ef08d4b5decadad47f4a95d35e2c54d676b83c0e
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-10-25 18:29:21 -0300

    Use fragment for reuse

--- react-graphql-github-apollo-starter-kit/src/Profile/index.js
+++ react-graphql-github-apollo-starter-kit/src/Profile/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import Loading from '../Loading';
-import RepositoryList from '../Repository';
+import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
 import ErrorMessage from '../Error';
 
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
@@ -14,30 +14,14 @@ const GET_REPOSITORIES_OF_CURRENT_USER = gql`
       ) {
         edges {
           node {
-            id
-            name
-            url
-            descriptionHTML
-            primaryLanguage {
-              name
-            }
-            owner {
-              login
-              url
-            }
-            stargazers {
-              totalCount
-            }
-            viewerHasStarred
-            watchers {
-              totalCount
-            }
-            viewerSubscription
+            ...repository
           }
         }
       }
     }
   }
+
+  ${REPOSITORY_FRAGMENT}
 `;
 
 const Profile = () => (
